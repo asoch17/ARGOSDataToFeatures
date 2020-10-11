dea121de663f9d9f3a4944e26010e9e108014067
Annika <as986@duke.edu>
2020-10-11 10:44:56 -0400

    iterate through all ARGOS files, ignoring the README.txt

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -16,7 +16,7 @@ import sys, os, arcpy
 arcpy.env.overwriteOutput = True
 
 # Set input variables (Hard-wired)
-inputFile = 'D:/AdvGIS/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+inputFolder = 'D:/AdvGIS/ARGOSTracking/Data/ARGOSData'
 outputFC = "D:/AdvGIS/ARGOSTracking/Scratch/ARGOStrack.shp"
 
 #set coordinate system
@@ -35,76 +35,85 @@ arcpy.AddField_management(outputFC,"Date","DATE")
 # Create the insert cursor
 cur = arcpy.da.InsertCursor(outputFC,['Shape@','TagID','LC','Date'])
 
-#%% Construct a while loop to iterate through all lines in the datafile
+#iterate through each ARGOS file
+inputFiles = os.listdir(inputFolder)
+for inputFile in inputFiles:
+    #don't process README.txt file
+    if inputFile == "README.txt":
+        continue
 
-# Open the ARGOS data file for reading
-inputFileObj = open(inputFile,'r')
+    #add full path to input file name
+    inputFile_full = os.path.join(inputFolder, inputFile)
 
-# Get the first line of data, so we can use a while loop
-lineString = inputFileObj.readline()
-
-# Start the while loop
-while lineString:
+    #Construct a while loop to iterate through all lines in the datafile
+    # Open the ARGOS data file for reading
+    inputFileObj = open(inputFile_full,'r')
     
-    # Set code to run only if the line contains the string "Date: "
-    if ("Date :" in lineString):
-        
-        # Parse the line into a list
-        lineData = lineString.split()
-        
-        # Extract attributes from the datum header line
-        tagID = lineData[0]
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsLC = lineData[7]
-        
-        # Extract location info from the next line
-        line2String = inputFileObj.readline()
-        
-        # Parse the line into a list
-        line2Data = line2String.split()
-        
-        # Extract the date we need to variables
-        obsLat = line2Data[2]
-        obsLon= line2Data[5]
-        
-         # Print results to see how we're doing
-       # print (tagID,"Lat:"+str(obsLat),"Long:"+str(obsLon),"Date:"+obsDate,"Time:"+obsTime,"LC:"+obsLC)
-        
-        #try to convert the coordinate numbers
-        try:
-        
-             # Convert raw coordinate strings to numbers
-            if obsLat[-1] == 'N':
-                obsLat = float(obsLat[:-1])
-            else:
-                obsLat = float(obsLat[:-1]) * -1
-            if obsLon[-1] == 'E':
-                obsLon = float(obsLon[:-1])
-            else:
-                obsLon = float(obsLon[:-1]) * -1
-                
-            # Construct a point object from the feature class
-            obsPoint = arcpy.Point()
-            obsPoint.X = obsLon
-            obsPoint.Y = obsLat
-            
-            # Convert the point to a point geometry object with spatial reference
-            inputSR = arcpy.SpatialReference(4326)
-            obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
-            
-             #add a feature using our insert cursor
-            feature = cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
-
-        #handle the error
-        except Exception as e:
-            print(f"Error adding recod {tagID} to the output")
-        
-    # Move to the next line so the while loop progresses
+    # Get the first line of data, so we can use a while loop
     lineString = inputFileObj.readline()
     
+    # Start the while loop
+    while lineString:
+        
+        # Set code to run only if the line contains the string "Date: "
+        if ("Date :" in lineString):
+            
+            # Parse the line into a list
+            lineData = lineString.split()
+            
+            # Extract attributes from the datum header line
+            tagID = lineData[0]
+            obsDate = lineData[3]
+            obsTime = lineData[4]
+            obsLC = lineData[7]
+            
+            # Extract location info from the next line
+            line2String = inputFileObj.readline()
+            
+            # Parse the line into a list
+            line2Data = line2String.split()
+            
+            # Extract the date we need to variables
+            obsLat = line2Data[2]
+            obsLon= line2Data[5]
+            
+             # Print results to see how we're doing
+           # print (tagID,"Lat:"+str(obsLat),"Long:"+str(obsLon),"Date:"+obsDate,"Time:"+obsTime,"LC:"+obsLC)
+            
+            #try to convert the coordinate numbers
+            try:
+            
+                 # Convert raw coordinate strings to numbers
+                if obsLat[-1] == 'N':
+                    obsLat = float(obsLat[:-1])
+                else:
+                    obsLat = float(obsLat[:-1]) * -1
+                if obsLon[-1] == 'E':
+                    obsLon = float(obsLon[:-1])
+                else:
+                    obsLon = float(obsLon[:-1]) * -1
+                    
+                # Construct a point object from the feature class
+                obsPoint = arcpy.Point()
+                obsPoint.X = obsLon
+                obsPoint.Y = obsLat
+                
+                # Convert the point to a point geometry object with spatial reference
+                inputSR = arcpy.SpatialReference(4326)
+                obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
+                
+                 #add a feature using our insert cursor
+                feature = cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
+    
+            #handle the error
+            except Exception as e:
+                print(f"Error adding recod {tagID} to the output")
+            
+        # Move to the next line so the while loop progresses
+        lineString = inputFileObj.readline()
+        
+    #Close the file object
+    inputFileObj.close()
+
 #Delete the cursor object
 del cur
-    
-#Close the file object
-inputFileObj.close()
